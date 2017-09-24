b33421cfc34826786f0f4502953802ba2476581b
Bryan Lopez <bryan13138@gmail.com>
2017-09-24 11:07:24 -0500

    Add callbacks for GPIO buttons

--- gpio-monitoring/gpio_monitoring.py
+++ gpio-monitoring/gpio_monitoring.py
@@ -11,33 +11,55 @@ def cmd(args):
     out = str(output.stdout.read())
     return out.split("\n")
 
+def callback_a(channel):
+    # Button press, try to initiate spaker-control
+    status = cmd(["sudo", "systemctl", "status", "speaker-control"])
+    running = False
+    for line in status:
+        if ("Active: " in line):
+            running = "active (running)" in line
+            break
 
-speaker_button = 40
+    if not running:
+        print("Start it")
+        cmd(["sudo", "systemctl", "restart", "speaker-control"])
+    else:
+        print("already running")
+    return
+
+def callback_c(channel):
+    print("C")
+    return
+
+def callback_d(channel):
+    print("D")
+    return
+
+def callback_e(channel):
+    print("E")
+    return
+
+def callback_f(channel):
+    print("F")
+    return
+
+
+pins = [3, 11, 13, 15, 19]
+callbacks = [callback_a, callback_c, callback_d, callback_e, callback_f]
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(speaker_button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+for pin in pins:
+    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+for pin, callback in zip(pins, callbacks):
+    GPIO.add_event_detect(pin, GPIO.FALLING, callback=callback, bouncetime=300)
 
 try:
     while True:
-        # Wait for button input
-        GPIO.wait_for_edge(speaker_button, GPIO.FALLING)
-
-        # Button press, try to initiate spaker-control
-        status = cmd(["sudo", "/etc/init.d/speaker-control.sh", "status"])
-        running = False
-        for line in status:
-            if ("Active: " in line):
-                running = "active (running)" in line
-                break
-
-        if not running:
-            cmd(["sudo", "/etc/init.d/speaker-control.sh", "restart"])
-            
-        sleep(1)
+        sleep(1e6)
 
 except Exception, e:
     GPIO.cleanup()
 
 GPIO.cleanup()
-
-
