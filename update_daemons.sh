717da57df2d5aa800fa5426c5116afd8c15aa18c
Bryan Lopez <bryan13138@gmail.com>
2017-09-24 11:02:03 -0500

    Change daemon mode to services

--- update_daemons.sh
+++ update_daemons.sh
@@ -1,23 +1,23 @@
-cd /media/USB_8/home-automation
+cd /home/osmc/code/home-automation/
 
-dos2unix ./speaker-control/speaker-control.sh
-dos2unix ./speaker-control/speaker_control.py
-dos2unix ./wemo-control/wemo-control.sh
-dos2unix ./wemo-control/wemo_control.py
-dos2unix ./gpio-monitoring/gpio-monitoring.sh
-dos2unix ./gpio-monitoring/gpio_monitoring.py
+sudo dos2unix ./speaker-control/speaker-control.service
+sudo dos2unix ./speaker-control/speaker_control.py
+sudo dos2unix ./wemo-control/wemo-control.service
+sudo dos2unix ./wemo-control/wemo_control.py
+sudo dos2unix ./gpio-monitoring/gpio-monitoring.service
+sudo dos2unix ./gpio-monitoring/gpio_monitoring.py
 
-chmod 755 ./speaker-control/speaker-control.sh
-chmod 755 ./speaker-control/speaker_control.py
-chmod 755 ./wemo-control/wemo-control.sh
-chmod 755 ./wemo-control/wemo_control.py
-chmod 755 ./gpio-monitoring/gpio-monitoring.sh
-chmod 755 ./gpio-monitoring/gpio_monitoring.py
+sudo chmod 664 ./speaker-control/speaker-control.service
+sudo chmod 755 ./speaker-control/speaker_control.py
+sudo chmod 664 ./wemo-control/wemo-control.service
+sudo chmod 755 ./wemo-control/wemo_control.py
+sudo chmod 664 ./gpio-monitoring/gpio-monitoring.service
+sudo chmod 755 ./gpio-monitoring/gpio_monitoring.py
 
-cp ./speaker-control/speaker-control.sh /etc/init.d/speaker-control.sh
-cp ./wemo-control/wemo-control.sh /etc/init.d/wemo-control.sh
-cp ./gpio-monitoring/gpio-monitoring.sh /etc/init.d/gpio-monitoring.sh
+sudo cp ./speaker-control/speaker-control.service /etc/systemd/system/speaker-control.service
+sudo cp ./wemo-control/wemo-control.service /etc/systemd/system/wemo-control.service
+sudo cp ./gpio-monitoring/gpio-monitoring.service /etc/systemd/system/gpio-monitoring.service
 
-update-rc.d speaker-control.sh defaults
-update-rc.d wemo-control.sh defaults
-update-rc.d gpio-monitoring.sh defaults
+sudo systemctl daemon-reload
+sudo systemctl enable wemo-control.service
+sudo systemctl enable gpio-monitoring.service
